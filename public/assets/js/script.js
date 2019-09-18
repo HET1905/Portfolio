423d41d55c5cc27b7ff0aef924d9c5ca9afb1b74
Hetal Patel <nphetal1905@gmail.com>
2019-09-18 12:22:34 -0400

    updated validation

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -20,14 +20,19 @@ $(document).ready(function(){
             email: email,
             client_message : client_message
         }
-        console.log(newContact);
         if(client_name===''){
             $('.errName').text('Insert your name');
             return false;
         }
+        else{
+            $('.errName').text(''); 
+        }
         if(email==='' || !isValidEmail()){
             $('.errEmail').text('Insert your valid email');
             return false;
+        }
+        else{
+            $('.errEmail').text('');
         }
         if(client_name !== '' && email !== ''){
             $.post('/api/contact',newContact,(err,data)=>{
@@ -44,13 +49,17 @@ $(document).ready(function(){
         if(dataSaved=== true){
             h2.text('I will contact you soon...Thank You');
             $('.formCol').append(h2);
+            $('#txtName').val('');
+            $('#txtEmail').val('');
+            $('#txtMessage').val('');
+
             // return false;   
         }
         
        
     });
+// Function to check valid email address
     function isValidEmail() {
-        console.log('inside fun');
         let email = $('#txtEmail').val().trim();
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(email);
